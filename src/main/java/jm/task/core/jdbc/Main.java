620bb0740422f0df16ab9d89bfb224268177edf7
NabokaEA <nabokae@yandex.ru>
2022-08-08 23:15:25 +0300

    try с ресурсами при установке сединеия + стайтмент

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,13 +4,19 @@ import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Connection connection = Util.getMySQLConnection();
-        if (!connection.isClosed()) {
-            System.out.println("Соединение установлено");
-            // реализуйте алгоритм здесь
+    public static void main(String[] args) {
+        try (Connection connection = Util.getMySQLConnection();
+             Statement statement = connection.createStatement()) {
+            if (!connection.isClosed()) {
+                System.out.println("Соединение установлено");
+            }
+        } catch (SQLException e) {
+            System.out.println("При установке соединения возникла исключительная ситуация");
+            e.printStackTrace();
         }
+
     }
 }
