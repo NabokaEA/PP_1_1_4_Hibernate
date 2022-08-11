4bdb33b27688deaaf320074e42b0076ad4a269f2
NabokaEA <nabokae@yandex.ru>
2022-08-11 21:26:23 +0300

    Version 1.0.0.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,28 +1,23 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        UserDaoJDBCImpl.getINSTANCE().dropUsersTable();
-        UserDaoJDBCImpl.getINSTANCE().createUsersTable();
-        UserDaoJDBCImpl.getINSTANCE().saveUser("Иван", "Иваныч", (byte) 15);
-        UserDaoJDBCImpl.getINSTANCE().saveUser("Иван", "Кузьмич", (byte) 17);
-        UserDaoJDBCImpl.getINSTANCE().saveUser("Петр", "Иваныч", (byte) 51);
-        UserDaoJDBCImpl.getINSTANCE().removeUserById(2);
-        List<User> users =  UserDaoJDBCImpl.getINSTANCE().getAllUsers();
-        for (User user: users)
-        System.out.println(user.toString());
-        UserDaoJDBCImpl.getINSTANCE().cleanUsersTable();
-        System.out.println("__________________________________");
-        users =  UserDaoJDBCImpl.getINSTANCE().getAllUsers();
-        for (User user: users)
+    public static void main(String[] args) {
+        UserServiceImpl service = new UserServiceImpl();
+        service.createUsersTable();
+        service.saveUser("Иван", "Иваныч", (byte) 15);
+        service.saveUser("Иван", "Кузьмич", (byte) 17);
+        service.saveUser("Петр", "Иваныч", (byte) 51);
+        service.saveUser("Дональд", "Иваныч", (byte) 59);
+        List<User> users = service.getAllUsers();
+        for (User user : users) {
             System.out.println(user.toString());
+        }
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -16,6 +16,7 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) {
         UserDaoJDBCImpl.getINSTANCE().saveUser(name, lastName, age);
+        System.out.printf("User с именем – 1%S добавлен в базу данных", name);
     }
 
     public void removeUserById(long id) {
